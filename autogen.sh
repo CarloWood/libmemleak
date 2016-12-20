c7107b54fa5fe017ec2ddf3d228562ae19d8bfe3
Carlo Wood <carlo@alinoe.com>
2016-12-20 19:47:23 +0100

    W.I.P. on autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -1,22 +1,24 @@
 #! /bin/sh
 
-# Clueless user check.
-if test ! -d .git -a -f configure; then
-  echo "You only need to run './autogen.sh' when you checked out this project from the git repository."
-  echo "Just run ./configure [--help]."
-  echo "If you insist on running it, then first remove the 'configure' script."
-  exit 0
-fi
-
-if test ! -e cwm4/scripts/bootstrap.sh; then
-  echo "*************************************************************************"
-  echo "* This project has a submodule (cwm4) and should be clone-d with:"
-  echo "* $ git clone --recursive $(git config --get remote.origin.url)"
-  echo "* Note the '--recursive'."
-  echo "*************************************************************************"
-  echo "Trying to recover..."
+if test -d .git; then
+  # Just always bring submodules up to date.
   git submodule init
   git submodule update
+else
+  # Clueless user check.
+  if -f configure; then
+    echo "You only need to run './autogen.sh' when you checked out this project from the git repository."
+    echo "Just run ./configure [--help]."
+    if test -e cwm4/scripts/bootstrap.sh; then
+      echo "If you insist on running it and know what you are doing, then first remove the 'configure' script."
+    fi
+    exit 0
+  elif test ! -e cwm4/scripts/bootstrap.sh
+    echo "Houston, we have a problem: the cwm4 git submodule is missing from your source tree.
+    echo "I'd suggest to clone the source code of this project from github:"
+    echo "git clone --recursive https://github.com/CarloWood/libmemleak.git"
+    exit 1
+  fi
 fi
 
 exec cwm4/scripts/bootstrap.sh
