2fa6fb3bdab0a423187251407185d93fe3a50f2c
Carlo Wood <carlo@alinoe.com>
2016-12-20 18:43:59 +0100

    W.I.P. on autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -1,1 +1,11 @@
-cwm4/scripts/autogen.sh
+#! /bin/sh
+
+# Clueless user check.
+if test ! -d .git -a -f configure; then
+  echo "You only need to run './autogen.sh' when you checked out this project from the git repository."
+  echo "Just run ./configure [--help]."
+  echo "If you insist on running it, then first remove the 'configure' script."
+  exit 0
+fi
+
+exec cwm4/scripts/bootstrap.sh
