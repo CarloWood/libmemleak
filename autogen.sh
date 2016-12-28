678e0f367bebba5fb8d3d0ea95fa1f4e1f665454
Carlo Wood <carlo@alinoe.com>
2016-12-28 19:16:18 +0100

    cwm4 update

--- autogen.sh
+++ autogen.sh
@@ -1,19 +1,31 @@
 #! /bin/sh
 
+if test -f .git; then
+  echo "Error: don't run $0 inside a submodule. Run it from the parent project's directory."
+  exit 1
+fi
+
+if test "$(realpath $0)" != "$(realpath $(pwd)/autogen.sh)"; then
+  echo "Error: run autogen.sh from the directory it resides in."
+  exit 1
+fi
+
 if test -d .git; then
+  # If this was a clone without --recursive, fix that fact.
+  if test ! -e cwm4/scripts/bootstrap.sh; then
+    git submodule update --init --recursive
+  fi
   # Update autogen.sh and cwm4 itself if we are the real maintainer.
   if test -f cwm4/scripts/real_maintainer.sh; then
     cwm4/scripts/real_maintainer.sh 15014aea5069544f695943cfe3a5348c
     RET=$?
-    # A return value of 2 means success. Otherwise abort/stop returning the same value.
+    # A return value of 2 means we need to continue with calling bootstrap.sh.
+    # Otherwise, abort/stop returning that value.
     if test $RET -ne 2; then
       echo "Exiting with code $RET"
       exit $RET
     fi
-  fi
-  # Always update the submodule(s).
-  git submodule init
-  if ! git submodule update --recursive; then
+  elif ! cwm4/scripts/update_submodules.sh --recursive; then
     echo "autogen.sh: Failed to update one or more submodules. Does it have uncommitted changes?"
     exit 1
   fi
@@ -27,7 +39,7 @@ else
     fi
     exit 0
   elif test ! -e cwm4/scripts/bootstrap.sh; then
-    echo "Houston, we have a problem: the cwm4 git submodule is missing from your source tree."
+    echo "Houston, we have a problem: the cwm4 git submodule is missing from your source tree!?"
     echo "I'd suggest to clone the source code of this project from github:"
     echo "git clone --recursive https://github.com/CarloWood/libmemleak.git"
     exit 1
